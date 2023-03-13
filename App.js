536bd80bf7d648a86a2ba6b2426ed7fd9e62702e
Anamika <116385962+Girianamika7277@users.noreply.github.com>
2023-03-14 01:26:14 +0530

    editer

--- App.js
+++ App.js
@@ -1,40 +1,48 @@
-let email = document.getElementById('Email');
-let password = document.getElementById('Password');
+let calculateBtn = document.getElementById('calculateBtn');
 
-function validated() {
-	// e.preventDefault();
-	if(emailIdVerify() && passwordVerify()){
-		return true;
-	}
-	else{
-		return false;
-	}
+let calculate = function(){
+    let totalBill   = document.getElementById('totalBill');
+    let servicePerc = document.getElementById('servicePerc');
+    let numPeople   = document.getElementById('numPeople');
+    let result      = document.getElementById('result');
+    let error = 0;
+        if (totalBill.value == "") {
+    	totalBill.style.borderColor = "#fc8370";
+    	error = 1;
+    }else {
+    	totalBill.style.borderColor = "#80a7b8";
+    	error = 0;
+    }
+    if (servicePerc.value == 0) {
+    	servicePerc.style.borderColor = "#fc8370";
+    	error = 1;
+    }else {
+    	servicePerc.style.borderColor = "#80a7b8";
+    	error = 0;
+    }
+    if (!error) {
 
+    	let tip = 0;
+    	let tipInit = totalBill.value * servicePerc.value;
+    	if (numPeople.value != 1 && numPeople.value != "") {
+            
+            let tipEach = tipInit / numPeople.value;
+
+            let tipEachRounded = Math.round(tipEach * 100) / 100;
+
+	        tipEachRounded = tipEachRounded.toFixed(2);
+
+	        tip = tipEachRounded == 0 ? tipEach + ' each' : tipEachRounded + ' each';
+    	}else {
+    		let tipRounded = Math.round(tipInit * 100) / 100;
+
+	        tipRounded = tipRounded.toFixed(2);
+	        tip = tipRounded == 0 ? tipInit : tipRounded;
+    	}
+        
+        result.innerText = tip;
+    }else {
+    	return;
+    }
 }
-
-let emailIdVerify = () => {
-	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
-		email.style.border = "1px solid silver";
-		email_error.style.display = "none";
-		return true;
-	} else {
-		email.style.border = "1px solid red";
-		email_error.style.display = "block";
-		email.focus();
-		return false;
-	}
-};
-
-let passwordVerify = ()=> {
-	if(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/.test(password.value)){
-		password.style.border = "1px solid silver";
-		pass_error.style.display = "none";
-		return true;
-	}
-	else {
-		password.style.border = "1px solid red";
-		pass_error.style.display = "block";
-		password.focus();
-		return false;
-	}
-}
+calculateBtn.addEventListener('click', calculate);
