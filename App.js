4a075a6e9c9927a03f4a8df17aead1826f2d0575
Anamika <116385962+Girianamika7277@users.noreply.github.com>
2023-06-28 09:56:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,100 +1,48 @@
-const quizData = [
-    {
-        question: "What is the most used programming language in 2019?",
-        a: "Java",
-        b: "C",
-        c: "Python",
-        d: "JavaScript",
-        correct: "d",
-    },
-    {
-        question: "Who is the President of US?",
-        a: "Florin Pop",
-        b: "Donald Trump",
-        c: "Ivan Saldano",
-        d: "Mihai Andrei",
-        correct: "b",
-    },
-    {
-        question: "What does HTML stand for?",
-        a: "Hypertext Markup Language",
-        b: "Cascading Style Sheet",
-        c: "Jason Object Notation",
-        d: "Helicopters Terminals Motorboats Lamborginis",
-        correct: "a",
-    },
-    {
-        question: "What year was JavaScript launched?",
-        a: "1996",
-        b: "1995",
-        c: "1994",
-        d: "none of the above",
-        correct: "b",
-    },
-];
+let calculateBtn = document.getElementById('calculateBtn');
 
-const quiz = document.getElementById("quiz");
-const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
-const submitBtn = document.getElementById("submit");
-
-let currentQuiz = 0;
-let score = 0;
-
-loadQuiz();
-
-function loadQuiz() {
-    deselectAnswers();
-
-    const currentQuizData = quizData[currentQuiz];
-
-    questionEl.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
-}
-
-function getSelected() {
-    let answer = undefined;
-
-    answerEls.forEach((answerEl) => {
-        if (answerEl.checked) {
-            answer = answerEl.id;
-        }
-    });
-
-    return answer;
-}
-
-function deselectAnswers() {
-    answerEls.forEach((answerEl) => {
-        answerEl.checked = false;
-    });
-}
-
-submitBtn.addEventListener("click", () => {
-    // check to see the answer
-    const answer = getSelected();
-
-    if (answer) {
-        if (answer === quizData[currentQuiz].correct) {
-            score++;
-        }
-
-        currentQuiz++;
-        if (currentQuiz < quizData.length) {
-            loadQuiz();
-        } else {
-            quiz.innerHTML = `
-                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
-                
-                <button onclick="location.reload()">Reload</button>
-            `;
-        }
+let calculate = function(){
+    let totalBill   = document.getElementById('totalBill');
+    let servicePerc = document.getElementById('servicePerc');
+    let numPeople   = document.getElementById('numPeople');
+    let result      = document.getElementById('result');
+    let error = 0;
+        if (totalBill.value == "") {
+    	totalBill.style.borderColor = "#fc8370";
+    	error = 1;
+    }else {
+    	totalBill.style.borderColor = "#80a7b8";
+    	error = 0;
     }
-});
+    if (servicePerc.value == 0) {
+    	servicePerc.style.borderColor = "#fc8370";
+    	error = 1;
+    }else {
+    	servicePerc.style.borderColor = "#80a7b8";
+    	error = 0;
+    }
+    if (!error) {
+
+    	let tip = 0;
+    	let tipInit = totalBill.value * servicePerc.value;
+    	if (numPeople.value != 1 && numPeople.value != "") {
+            
+            let tipEach = tipInit / numPeople.value;
+
+            let tipEachRounded = Math.round(tipEach * 100) / 100;
+
+	        tipEachRounded = tipEachRounded.toFixed(2);
+
+	        tip = tipEachRounded == 0 ? tipEach + ' each' : tipEachRounded + ' each';
+    	}else {
+    		let tipRounded = Math.round(tipInit * 100) / 100;
+
+	        tipRounded = tipRounded.toFixed(2);
+	        tip = tipRounded == 0 ? tipInit : tipRounded;
+    	}
+        
+        result.innerText = tip;
+    }else {
+    	return;
+    }
+}
+calculateBtn.addEventListener('click', calculate);
